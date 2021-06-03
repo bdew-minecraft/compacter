2b8e7f508ea06dd76e9b2e1ba14ab7a8058d44f3
bdew <blackdew@gmail.com>
2021-06-03 09:21:55 +0300

    set mod id in NetChannel explicitly to prevent fix order shenanigans (bdew-minecraft/bdlib#53)

--- src/main/scala/net/bdew/compacter/network/NetworkHandler.scala
+++ src/main/scala/net/bdew/compacter/network/NetworkHandler.scala
@@ -1,9 +1,10 @@
 package net.bdew.compacter.network
 
+import net.bdew.compacter.Compacter
 import net.bdew.compacter.blocks.compacter.ContainerCompacter
 import net.bdew.lib.network.NetChannel
 
-object NetworkHandler extends NetChannel("main", "2") {
+object NetworkHandler extends NetChannel(Compacter.ModId, "main", "2") {
   regServerContainerHandler(1, CodecSetRsMode, classOf[ContainerCompacter]) { (msg, cont, _) =>
     cont.te.rsMode := msg.rsMode
     cont.te.haveWork = true
